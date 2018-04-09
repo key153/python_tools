105b3c25627406cc697f57031d891b8917f6a883
konghao <kongh999@163.com>
2018-04-09 17:28:14 +0800

    add py2exe modules

---
+++ py2exe.py
@@ -0,0 +1,12 @@
+# setup.py代码
+# from distutils.core import setup
+# import py2exe
+# setup(console=["将要转换的文件名称.py"])
+#
+# setup.py里修改文件名
+# python setup.py py2exe
+
+from distutils.core import setup
+import py2exe
+
+setup(console=["test.py"])
